40580356ffc6af42bc0b0ae299ce147b74fa4ee5
asd1025 <tmwrsts@daum.net>
2019-06-14 19:35:10 +0900

    .

--- problem05.py
+++ problem05.py
@@ -2,11 +2,10 @@
 # 선택정렬을 적용하는 코드
 a= [5,9,3,8,60,20,1]
 print('Before sort.\n',a)
-for i,before in enumerate(a) :
-    for j,after in  enumerate(a):
-        if(i<j and a[i]<a[j]) :
+for i in  range (0,len(a)) :
+    for j in range (0,len(a)-(i+1)) :
+        if(a[j+1]>a[j]) :
             temp = a[j]
-            a[j]=a[i]
-            a[i]=temp
+            a[j] = a[j + 1]
+            a[j + 1] = temp
 print('After sort.\n',a)
- 
